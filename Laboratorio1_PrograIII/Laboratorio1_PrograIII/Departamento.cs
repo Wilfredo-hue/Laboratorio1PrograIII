525b6acb289b2a32120cea27cd30410e850827b4
Wilfredo-hue <78829758+Wilfredo-hue@users.noreply.github.com>
2023-02-22 15:02:18 -0600

    Es para lo que da mi cerebro!!!

---
+++ Laboratorio1_PrograIII/Laboratorio1_PrograIII/Departamento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1_PrograIII
+{
+    public  class Departamento : Municipio
+    {
+        private string nombre;
+        private Municipio municipio;
+
+        public string Nombre
+        {
+            get => Nombre; 
+            set => Nombre = value; 
+        }
+        public string Municipio 
+        { 
+            get => Municipio;
+            set => Municipio = value;
+        }
+    }
+}
---
+++ Laboratorio1_PrograIII/Laboratorio1_PrograIII/Personas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1_PrograIII
+{
+    public abstract class Persona
+    {
+        private string nombre;
+        private int edad;
+        private Departamento departamento;
+        private Municipio municipio;
+
+
+        public int Nombre
+        {
+            get => Edad;
+            set => Edad = value;
+        }
+        public int Edad
+        { 
+            get => Edad;
+            set => Edad = value; 
+        }
+        public Departamento Departamento 
+        { 
+            get => departamento; 
+            set => departamento = value;
+        }
+        public Municipio Municipio 
+        { 
+            get => municipio; 
+            set => municipio = value;
+        }
+
+    }
+}
